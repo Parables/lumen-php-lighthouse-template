157725a39aa2f546bf0b06ddabd4885ddaa2ca52
Parables Boltnoel <parables95@gmail.com>
2021-01-05 17:20:47 +0000

    WIP.. adding models and migrations

--- database/migrations/2021_01_05_140535_create_payment_records_table.php
+++ database/migrations/2021_01_05_140535_create_payment_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBookCourseTable extends Migration
+class CreatePaymentRecordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateBookCourseTable extends Migration
      */
     public function up()
     {
-        Schema::create('book_course', function (Blueprint $table) {
-            $table->foreignId('book_id')->compact();
-            $table->foreignId('course_id')->compact();
+        Schema::create('payment_records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -26,6 +26,6 @@ class CreateBookCourseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('book_course');
+        Schema::dropIfExists('payment_records');
     }
 }
