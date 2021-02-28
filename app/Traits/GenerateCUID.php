883b14551d7e676446a3e81781e4e4ab935cbf82
parables <parables95@gmail.com>
2021-02-28 08:48:53 +0000

    Fixed CUID issues with nested mutations

--- app/Traits/GenerateCUID.php
+++ app/Traits/GenerateCUID.php
@@ -2,18 +2,42 @@
 
 namespace App\Traits;
 
-// use Illuminate\Support\Str;
-
 use EndyJasmi\Cuid;
 
+/**
+ * Generates Collision-resistance Unique ID
+ */
 trait GenerateCUID
 {
-    protected static function bootUsesUuid()
+    /**
+     * Generate CUID when creating model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::cuid();
+    }
+
+    /**
+     * Defines the UUID field for the model.
+     * @return string
+     */
+    protected static function uuidField()
+    {
+        return 'id';
+    }
+
+    /**
+     * Use if boot() is overridden in the model.
+     */
+    protected static function cuid()
     {
         static::creating(function ($model) {
-            if (!$model->getKey()) {
-                $model->{$model->getKeyName()} = (string) Cuid::cuid();
-            }
+            $model->{self::uuidField()} = Cuid::cuid();
+            $model->keyType = 'string';
+            $model->incrementing = false;
+            $model->{$model->getKeyName()} = $model->{$model->getKeyName()} ?: (string) Cuid::cuid();
         });
     }
 
